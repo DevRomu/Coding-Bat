ff4c386b381d170882a56c41be347752317fc165
Ro <orinarhomill@gmail.com>
2022-01-18 23:10:17 -0600

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# Coding-Bat
+ Coding Bat Problems That I will solve
---
+++ codingBatProblem1.java
@@ -0,0 +1,26 @@
+/** 
+ * Coding Bat warmup if and else statements 
+ * Return false if the given string is sleepin
+ * 
+ * Author:Rhomill
+ * Date: Jan 18th, 2022
+ */
+
+public class codingBatProblem1 {
+
+  public static void main(String[] args) {
+
+    public boolean sleepIn(boolean weekday, boolean vacation){
+        if(weekday & vacation){
+        return true;
+        }
+        else if(!weekday & vacation){
+            
+        }
+      
+        System.out.println(sleepIn);
+      }
+        
+  }
+
+}
