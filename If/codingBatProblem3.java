a313fcb262f7bb125fabf62952b151309ef64b1e
Ro <orinarhomill@gmail.com>
2022-01-31 22:33:32 -0600

    Create codingBatProblem3.java
    New if problem I'm trying to solve

---
+++ If/codingBatProblem3.java
@@ -0,0 +1,33 @@
+
+/** 
+ * Coding Bat warmup if and else statements.
+ * Determines when you are allowed to sleep-in.
+ * 
+ * @author DevRomu
+ * @version 1.0.0
+ */
+public class codingBatProblem3 {
+  public static void main(String[] args)
+  {
+    boolean aSmile = true; 
+    boolean bSmile = false;
+    
+    
+    // TODO: Code all the unit tests detailed on CodingBat.
+    System.out.println(sumDouble(a, b));
+  }
+ 
+ /**
+  * TODO: JavaDoc
+
+  * @param a
+  * @param b
+  */
+  public static void sumDouble(int a, int b)
+  { 
+    if( int a && int b )
+    {
+
+    }
+  }
+}
