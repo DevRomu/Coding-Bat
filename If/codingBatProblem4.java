25ac76370ff0d28fc0ea6f0ff5eae059fd39fa2b
Ro <orinarhomill@gmail.com>
2022-02-07 23:39:02 -0600

    Made changes
    Made changes to the solution

---
+++ If/codingBatProblem4.java
@@ -0,0 +1,33 @@
+
+/** 
+ * Coding Bat warmup if and else statements.
+ * Determines when you are allowed to sleep-in.
+ * 
+ * @author DevRomu
+ * @version 1.0.0
+ */
+public class codingBatProblem4 
+{
+  public static void main(String[] args)
+  {
+    // TODO: Code all the unit tests detailed on CodingBat.
+    System.out.println(sum);
+  }
+ 
+ /**
+  * TODO: JavaDoc
+
+  * @param a
+  * @param b
+ * @return 
+  */
+  public static boolean sumDouble(int a, int b, int sum)
+  { 
+    if (a == b) 
+    {
+      sum = sum * 2;
+    } 
+    return sum;
+  }
+ 
+}
